f76e217c0c573b7b869a47b0709c43fab535cdb3
Enrique Ortiz <hi@enriqueortiz.dev>
2020-09-14 18:44:36 -0400

    Add package for disputable delay + initial setup

--- src/Apps/Apps.js
+++ src/Apps/Apps.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { useParams } from 'react-router-dom'
 import { Header } from '@aragon/ui'
-import Agreement from './Agreement/Agreement'
 
-const KNOWN_APPS = new Map([['agreement', Agreement]])
+// Frontends
+import Agreement from './Agreement/Agreement'
+import DisputableDelay from './DisputableDelay/DisputableDelay'
+
+const KNOWN_APPS = new Map([
+  ['agreement', Agreement],
+  ['agreement-1hive', Agreement],
+  ['disputable-delay', DisputableDelay],
+])
 
 export default function Apps({ apps }) {
   const { appAddress } = useParams()
@@ -12,12 +19,15 @@ export default function Apps({ apps }) {
   const matchingApp = apps.find(({ address }) => address === appAddress)
   const matchingView = KNOWN_APPS.get(matchingApp.name)
 
+  const MatchingView = useMemo(() => matchingView, [matchingView])
+  const title = useMemo(
+    () => matchingApp.name.replace(/\b\w/g, c => c.toUpperCase()),
+    [matchingApp.name],
+  )
+
   if (!matchingView) {
     return <Header primary="Generic View" />
   }
-
-  const MatchingView = matchingView
-  const title = matchingApp.name.replace(/\b\w/g, c => c.toUpperCase())
 
   return (
     <>
---
+++ src/Apps/DisputableDelay/DisputableDelay.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import connectDisputableDelay from '@1hive/connect-disputable-delay'
+import 'styled-components/macro'
+
+export default function DisputableDelay() {
+  return (
+    <div>
+      <h2>Disputable delay</h2>
+    </div>
+  )
+}
